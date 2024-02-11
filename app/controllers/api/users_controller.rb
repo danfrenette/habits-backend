d4e57deebc56aadfe615c45c4a655d79f452770e
Dan Frenette <dan.r.frenette@gmail.com>
2024-02-11 14:18:40 -0500

    Configure CORS

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,5 @@
+class Api::UsersController < ApplicationController
+  def create
+    puts "params: #{params}"
+  end
+end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,15 @@
-# Be sure to restart your server when you modify this file.
+Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: false, logger: (-> { Rails.logger }) do
+  cors_origins = ENV.fetch("CORS_ORIGINS", "localhost:3000")
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
+  if cors_origins && cors_origins != "*"
+    allow do
+      origins(*cors_origins.split(",").map(&:strip))
 
-# Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+      resource "/api/*",
+        headers: :any,
+        methods: [:get, :post, :delete, :put, :patch, :options, :head],
+        max_age: 0,
+        credentials: true
+    end
+  end
+end
